a3d2de2c79a9e95761b245fca9acb57ce4893880
Shakti <113392943+El-47@users.noreply.github.com>
2023-03-28 12:15:21 +0530

    remove security key

--- Django project/pro5/TODO/TODO/settings.py
+++ Django project/pro5/TODO/TODO/settings.py
@@ -9,8 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4j9tjht=7ubl$lah9+3yl9s(3_2h$75%a62y!bndby9b3i29df'
+# SECURITY WARNING: keep the secret key used in production secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
